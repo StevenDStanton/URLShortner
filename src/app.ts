7ac83061d7d264e458bd5c8ac5645f18c449f118
Steven Stanton <steven.stanton@gmail.com>
2024-02-26 21:27:07 -0500

    shortenedURL

--- src/app.ts
+++ src/app.ts
@@ -23,8 +23,7 @@ if (port === undefined) {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/api/url', urlRouter);
+app.use('/', urlRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express';
-
-const router: Router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-  res.send({ status: 'I Am Alive!' });
-});
-
-export { router as indexRouter };
