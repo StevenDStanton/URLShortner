a715b3e046472e24cd0fef0ce41a1bae2bc85f5b
sstanton <steven@thesimpledev.com>
2024-07-18 17:11:20 -0400

    Setup deployment

--- aws-cdk/aws-cdk.go
+++ aws-cdk/aws-cdk.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awscertificatemanager"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsroute53"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsroute53targets"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3assets"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
+	"github.com/joho/godotenv"
 )
 
 type AwsCdkStackProps struct {
@@ -18,11 +27,66 @@ func NewAwsCdkStack(scope constructs.Construct, id string, props *AwsCdkStackPro
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	awslambda.NewFunction(stack, jsii.String("urlshortnergo"), &awslambda.FunctionProps{
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	domainName := os.Getenv("DOMAIN_NAME")
+	subdomainName := os.Getenv("SUBDOMAIN_NAME")
+
+	// Create the Lambda function
+	lambdaFunction := awslambda.NewFunction(stack, jsii.String("urlshortnergo"), &awslambda.FunctionProps{
 		Runtime:      awslambda.Runtime_PROVIDED_AL2023(),
-		Code:         awslambda.AssetCode_FromAsset(jsii.String("../build/function.zip"), nil),
+		Code:         awslambda.AssetCode_FromAsset(jsii.String("../build/function.zip"), &awss3assets.AssetOptions{}),
 		Handler:      jsii.String("main"),
 		FunctionName: jsii.String("urlshortnergo"),
+		Environment:  &map[string]*string{
+			// Add necessary environment variables if needed
+		},
+	})
+
+	// Lookup the existing Route 53 hosted zone
+	hostedZone := awsroute53.HostedZone_FromLookup(stack, jsii.String("HostedZone"), &awsroute53.HostedZoneProviderProps{
+		DomainName: jsii.String(domainName),
+	})
+
+	// Request an SSL certificate
+	certificate := awscertificatemanager.NewCertificate(stack, jsii.String("Certificate"), &awscertificatemanager.CertificateProps{
+		DomainName: jsii.String(subdomainName),
+		Validation: awscertificatemanager.CertificateValidation_FromDns(hostedZone),
+	})
+
+	// Create the API Gateway with the custom domain name
+	api := awsapigateway.NewRestApi(stack, jsii.String("UrlShortenerApi"), &awsapigateway.RestApiProps{
+		DefaultCorsPreflightOptions: &awsapigateway.CorsOptions{
+			AllowHeaders: jsii.Strings("Content-Type", "Authorization"),
+			AllowMethods: jsii.Strings("POST", "GET", "PUT", "OPTIONS"),
+			AllowOrigins: jsii.Strings("*"),
+		},
+		DeployOptions: &awsapigateway.StageOptions{
+			LoggingLevel: awsapigateway.MethodLoggingLevel_INFO,
+		},
+		CloudWatchRole: jsii.Bool(true),
+		DomainName: &awsapigateway.DomainNameOptions{
+			Certificate: certificate,
+			DomainName:  jsii.String(subdomainName),
+		},
+	})
+
+	integration := awsapigateway.NewLambdaIntegration(lambdaFunction, nil)
+
+	// Define Routes
+	api.Root().AddResource(jsii.String("healthCheck"), nil).AddMethod(jsii.String("GET"), integration, nil)
+	api.Root().AddResource(jsii.String("shorten"), nil).AddMethod(jsii.String("PUT"), integration, nil)
+	api.Root().AddMethod(jsii.String("GET"), integration, nil)
+
+	// Configure Route 53 A record
+	awsroute53.NewARecord(stack, jsii.String("ApiAliasRecord"), &awsroute53.ARecordProps{
+		Zone:       hostedZone,
+		RecordName: jsii.String("s"), // Subdomain
+		Target:     awsroute53.RecordTarget_FromAlias(awsroute53targets.NewApiGateway(api)),
 	})
 
 	return stack
@@ -43,6 +107,13 @@ func main() {
 }
 
 func env() *awscdk.Environment {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
-	return nil
+	return &awscdk.Environment{
+		Account: jsii.String(os.Getenv("AWS_ACCOUNT_ID")),
+		Region:  jsii.String(os.Getenv("AWS_REGION")),
+	}
 }
--- aws-cdk/go.mod
+++ aws-cdk/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-cdk-go/awscdk/v2 v2.149.0
 	github.com/aws/constructs-go/constructs/v10 v10.3.0
 	github.com/aws/jsii-runtime-go v1.101.0
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
